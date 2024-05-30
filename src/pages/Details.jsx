21453656ac1a4b56492f7b859ddafed9331c5d05
thergupta2001 <2001.rohangupta@gmail.com>
2024-05-30 15:37:50 +0530

    minor change

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -12,7 +12,7 @@ const Details = () => {
             try {
                 const response = await fetch('https://8b648f3c-b624-4ceb-9e7b-8028b7df0ad0.mock.pstmn.io/dishes/v1/1');
                 const data = await response.json();
-                console.log(data);
+
                 setDetails(data);
             } catch (error) {
                 console.error('Error fetching data:', error);
